a59ed75566a4caff0bf452e108146557577bc616
sanbo.xyz <sanbo.xyz@gmail.com>
2021-10-12 14:27:06 +0800

    update

---
+++ LogLib/src/androidTest/java/me/hhhaiai/log/LogsTest.java
@@ -0,0 +1,26 @@
+package me.hhhaiai.log;
+
+
+public class LogsTest extends BaseLogTest {
+
+
+    public void testV() {
+    }
+
+    public void testD() {
+    }
+
+    public void testI() {
+        Logs.i("test");
+        System.err.println("xxxxx");
+    }
+
+    public void testW() {
+    }
+
+    public void testE() {
+    }
+
+    public void testWtf() {
+    }
+}
