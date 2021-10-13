50d0f774fd5efa0aa41a7816f1c6cfaa5680681b
sanbo.xyz <sanbo.xyz@gmail.com>
2021-10-13 14:14:31 +0800

    add cases

--- LogLib/src/androidTest/java/me/hhhaiai/log/LogsTest.java
+++ LogLib/src/androidTest/java/me/hhhaiai/log/LogsTest.java
@@ -3,11 +3,12 @@ package me.hhhaiai.log;
 
 public class LogsTest extends BaseLogTest {
 
-
     public void testV() {
+        Logs.v("test");
     }
 
     public void testD() {
+        Logs.d("test");
     }
 
     public void testI() {
@@ -16,11 +17,14 @@ public class LogsTest extends BaseLogTest {
     }
 
     public void testW() {
+        Logs.w("test");
     }
 
     public void testE() {
+        Logs.e("test");
     }
 
     public void testWtf() {
+        Logs.wtf("test");
     }
 }
