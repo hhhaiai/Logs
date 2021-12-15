1d75d53c888e44c9e64c551b854b2b7cc0191936
sanbo <sanbo.xyz@gmail.com>
2021-12-15 18:15:16 +0800

    update sth. at 2021-12-15 18:15:16

---
+++ LogLib/src/androidTest/java/me/hhhaiai/log/parsers/ClassParserTest.java
@@ -0,0 +1,24 @@
+package me.hhhaiai.log.parsers;
+
+import junit.framework.TestCase;
+
+/**
+ * @Copyright © 2021 sanbo Inc. All rights reserved.
+ * @Description: TODO
+ * @Version: 1.0
+ * @Create: 2021/12/13 11:08 AM
+ * @author: sanbo
+ */
+public class ClassParserTest extends TestCase {
+
+    ClassParser cla =new ClassParser();
+
+    public void testParserObject() {
+    }
+
+    public void testParserMethodByClass() {
+    }
+
+    public void testParserFieldByClass() {
+    }
+}
